c846e9e4be3da1103746b7481f1f1f46533fcea7
Babay <babay@mail.ru>
2018-05-11 00:41:12 +0300

    added crashlytics

--- app/src/main/java/com/cryptaur/lottery/TheApplication.java
+++ app/src/main/java/com/cryptaur/lottery/TheApplication.java
@@ -2,10 +2,12 @@ package com.cryptaur.lottery;
 
 import android.app.Application;
 
+import com.crashlytics.android.Crashlytics;
 import com.cryptaur.lottery.model.TransactionKeeper;
 import com.cryptaur.lottery.transport.SessionTransport;
 import com.jakewharton.threetenabp.AndroidThreeTen;
 
+import io.fabric.sdk.android.Fabric;
 import io.paperdb.Paper;
 
 public class TheApplication extends Application {
@@ -13,6 +15,9 @@ public class TheApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        if (!BuildConfig.DEBUG)
+            Fabric.with(this, new Crashlytics());
+
         AndroidThreeTen.init(this);
         SessionTransport.INSTANCE.initContext(getApplicationContext());
         Paper.init(this);
